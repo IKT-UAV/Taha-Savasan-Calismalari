6348cf7167db2a006c00da5a80cb32fa12631eb7
taharidvanozturk <taharidvanozturk0@gmail.com>
2023-12-18 12:48:47 +0300

    isim duzeltme

--- api.py
+++ api.py
@@ -1,11 +1,14 @@
+from datetime import datetime
+
 import uvicorn
 from fastapi import FastAPI, HTTPException, Depends
-from datetime import datetime
 from pydantic import BaseModel
+
 
 class GirisData(BaseModel):
     kadi: str
     sifre: str
+
 
 class UserSession:
     def __init__(self):
@@ -27,13 +30,16 @@ class UserSession:
     def get_authenticated_user(self):
         return self.authenticated_user
 
+
 user_session = UserSession()
 app = FastAPI()
+
 
 def get_current_user():
     if not user_session.is_authenticated():
         raise HTTPException(status_code=401, detail="Yetkisiz giris denemesi")
     return user_session.get_authenticated_user()
+
 
 @app.post("/giris")
 async def login(giris_data: GirisData):
@@ -42,26 +48,32 @@ async def login(giris_data: GirisData):
     else:
         raise HTTPException(status_code=401, detail="Yetkisiz giris denemesi")
 
+
 @app.get("/sunucusaati")
 async def root(current_user: str = Depends(get_current_user)):
     return {"sunucusaati": datetime.now().isoformat()}
+
 
 # Apply the same dependency to other endpoints
 @app.post("/telemetri_gonder")
 async def say_hello(current_user: str = Depends(get_current_user)):
     return {}
 
+
 @app.post("/kilitlenme_bilgisi")
 async def say_hello(current_user: str = Depends(get_current_user)):
     return {}
+
 
 @app.post("/kamikaze_bilgisi")
 async def say_hello(current_user: str = Depends(get_current_user)):
     return {}
 
+
 @app.get("/qr_koordinati")
 async def say_hello(current_user: str = Depends(get_current_user)):
     return {"enlem": "51", "boylam": "53,123456,11"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="192.168.1.199", port=8080)
